a54aaba7d72a21d983541601b2c01dddc255ef88
Hazem18 <139848488+Hazem18@users.noreply.github.com>
2024-07-06 11:08:35 +0300

    Add files via upload

---
+++ Task1/Carpet Cleaning Service/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Carpet_Cleaning_Service
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int smallCarpet = 25;
+            int largeCarpet = 35;
+            double taxRate = 0.06;
+
+            Console.Write("Number of small carpets:");
+            int smallQuantity = int.Parse(Console.ReadLine());
+
+            Console.Write("Number of large carpets:");
+            int largeQuantity = int.Parse(Console.ReadLine());
+
+            if(smallQuantity < 0 || largeQuantity < 0 )
+              Console.WriteLine("Invalid input...Can't input negative values.");
+
+            else
+            {
+                int cost = (smallCarpet * smallQuantity) + (largeCarpet * largeQuantity);
+                double netCost = (cost * taxRate) + cost;
+                
+                Console.WriteLine($"Total estimate: {netCost}");
+                Console.WriteLine("This estimate is valid for 30 days.");
+            }
+
+        }
+    }
+}
